39f32bb38c2e0f4ab859a858954104d7eb7a5782
JEONSEUNGREE <jws08219@naver.com>
2023-07-01 14:54:16 +0900

    security_init_configure_1

---
+++ spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -0,0 +1,21 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
+        http.formLogin();
+        http.apply(new CustomSecurityConfigurer().setFlag(false));
+        return http.build();
+    }
+
+
+}
