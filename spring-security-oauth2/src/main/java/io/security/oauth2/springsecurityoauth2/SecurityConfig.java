ca943be8d25568978c82a8332417b5f7eb9e4d5f
JEONSEUNGREE <jws08219@naver.com>
2023-07-08 17:41:32 +0900

    oauth2.0client dependency 추가

--- spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -1,35 +1,22 @@
-package io.security.oauth2.springsecurityoauth2;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.SecurityFilterChain;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated();
-        http.formLogin();
-        http.httpBasic();
-
-//        http.exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
-//            @Override
-//            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-//                System.out.println("custom entrypoint");
-//            }
-//        });
-        return http.build();
-    }
-
-
-}
+//package io.security.oauth2.springsecurityoauth2;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().anyRequest().authenticated();
+//        http.formLogin();
+//        http.apply(new CustomSecurityConfigurer().setFlag(false));
+//
+//        return http.build();
+//    }
+//
+//
+//}
