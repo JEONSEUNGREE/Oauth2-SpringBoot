fb9c9d1c2c582e5a75e384c46a8006fff0e43640
JEONSEUNGREE <jws08219@naver.com>
2023-07-15 16:10:47 +0900

    인증객체 구분

--- spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/LoginController.java
+++ spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/LoginController.java
@@ -2,6 +2,10 @@ package io.security.oauth2.springsecurityoauth2;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -11,6 +15,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,21 +33,54 @@ public class LoginController {
 
     @GetMapping("/loginPage")
     public String loginPage() {
+
         return "loginPage";
     }
 
+//    @GetMapping("/user")
+//    public OAuth2User user(@RequestParam String accessToken) {
+//
+//        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
+//        OAuth2AccessToken oAuth2AccessToken = new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER, accessToken, Instant.now(),Instant.MAX);
+//
+//        OAuth2UserRequest oAuth2UserRequest = new OAuth2UserRequest(clientRegistration, oAuth2AccessToken);
+//        DefaultOAuth2UserService defaultOAuth2UserService = new DefaultOAuth2UserService();
+//        OAuth2User oAuth2User = defaultOAuth2UserService.loadUser(oAuth2UserRequest);
+//
+//        return oAuth2User;
+//    }
+//
     @GetMapping("/user")
-    public OAuth2User user(@RequestParam String accessToken) {
+    public Authentication user(Authentication authentication) {
 
-        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
-        OAuth2AccessToken oAuth2AccessToken = new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER, accessToken, Instant.now(),Instant.MAX);
+        OAuth2AuthenticationToken authentication2 = (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
 
-        OAuth2UserRequest oAuth2UserRequest = new OAuth2UserRequest(clientRegistration, oAuth2AccessToken);
-        DefaultOAuth2UserService defaultOAuth2UserService = new DefaultOAuth2UserService();
-        OAuth2User oAuth2User = defaultOAuth2UserService.loadUser(oAuth2UserRequest);
+        OAuth2AuthenticationToken authentication1 = (OAuth2AuthenticationToken) authentication;
+
+        OAuth2User oAuth2User = (OAuth2User) authentication2.getPrincipal();
+
+        return authentication2;
+    }
+
+    // 같은 OAuth2User 상속받지만 명확한 인증방법의 구분을 위해서 OidcUser 사용
+    @GetMapping("/oauth2User")
+    public OAuth2User user(@AuthenticationPrincipal OAuth2User oAuth2User) {
+
+        // oauth2.0 provider 방식으로 받은경우
+        System.out.println("oAuth2User = " + oAuth2User);
 
         return oAuth2User;
     }
+
+    @GetMapping("/oidcUser")
+    public OAuth2User user(@AuthenticationPrincipal OidcUser oidcUser) {
+
+        // openId connect 방식으로 인증을 받은 경우
+        System.out.println("oidcUser = " + oidcUser);
+
+        return oidcUser;
+    }
+
 
     @GetMapping("/oidc")
     public OAuth2User oidc(@RequestParam String accessToken, String idToken) {
--- spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,22 +1,38 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 public class OAuth2ClientConfig {
 
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authRequest ->
-                authRequest
-                        .anyRequest() .permitAll()
-                        );
-        // 로그인 페이지를 변경
-        http.oauth2Login(oauth2 -> oauth2.loginPage("/loginPage"));
-
+        http.authorizeRequests(authRequest -> authRequest.anyRequest().authenticated());
+        http.oauth2Login(Customizer.withDefaults());
+        http.logout()
+                .logoutSuccessHandler(oidcLogoutSuccessHandler())
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID");
+// Customizer 인터페이스 안에 withDefaults()로 두면 커스텀한 설정을 하지않겠다는 의미이다.
         return http.build();
+    }
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8081/login");
+
+        return successHandler;
     }
 }
